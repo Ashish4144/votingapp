d460dfc827d85cf41a91f8dbbd3fc9c4c92a2cb2
Ashish4144 <mishra122334@gmail.com>
2024-09-22 23:33:44 +0530

    After Smart Contracts and deploy.js these are the necessary changes

---
+++ context/voter.js
@@ -0,0 +1,12 @@
+import React, {useState,useEffect} from 'react';
+import Web3Modal from 'web3modal';
+
+const voter = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default voter
